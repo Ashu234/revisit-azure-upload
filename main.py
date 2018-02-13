f6b65be215b2fc2bd8acf508c2d19f952a00a55c
Ashutosh Singh <ashu.singh234@gmail.com>
2018-02-13 14:16:07 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return '''
-    <!doctype html>
-      <title>Upload new File</title>
-      <h1>Upload new File</h1>
-      <form action="" method=post enctype=multipart/form-data>
-        <p><input type=file name=file>
-         <input type=submit value=Upload>
-      </form>
-    '''
+  return render_template('home.html')
 
 if __name__ == '__main__':
   app.run()
