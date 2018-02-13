bbc75d8d747e87af649c5fe5567aff30b72ec448
Ashutosh Singh <ashu.singh234@gmail.com>
2018-02-13 14:58:56 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,11 @@ app = Flask(__name__)
 def hello_world():
   return render_template('home.html')
 
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        return render_template('complete.html')
+    return render_template('upload.html')
+
 if __name__ == '__main__':
   app.run()
