c8dbeb70adc5e2513ea34f195d048ca9c1a64288
Ashutosh Singh <ashu.singh234@gmail.com>
2018-02-15 00:06:23 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for
 from azure.storage.blob import BlockBlobService
 from azure.storage.blob import PublicAccess
 from azure.storage.blob import ContentSettings
@@ -27,8 +27,6 @@ def index():
 def login():
     if request.method == 'POST':
         username = request.form['username']
-        session['logged_in'] = True
-        session['username'] = username
         return redirect(url_for('dashboard'))
     return render_template('login.html')
 
@@ -39,34 +37,6 @@ def dashboard():
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
-        username = session['username']
-        try:
-           conn = mysql.connector.connect(**config)
-           print("Connection established")
-        except mysql.connector.Error as err:
-          if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            print("Something is wrong with the user name or password")
-          elif err.errno == errorcode.ER_BAD_DB_ERROR:
-            print("Database does not exist")
-          else:
-            print(err)
-        else:
-          cursor = conn.cursor()
-          for file in request.files.getlist("file"):
-                title = request.form['title']
-                file_to_upload = file.filename
-                full_path_to_file = os.path.join(os.path.dirname(__file__), file_to_upload)
-                block_blob_service.create_blob_from_path(
-                'ashu-blob-container',
-                title,
-                full_path_to_file,
-                content_settings=ContentSettings(content_type='image/png')
-                )
-                cursor.execute("INSERT INTO images (username, title) VALUES (%s, %s);", (username, title))
-                print("Inserted",cursor.rowcount,"row(s) of data.")
-          conn.commit()
-          cursor.close()
-          conn.close()
         return render_template('complete.html')
     return render_template('upload.html')
 
@@ -103,4 +73,3 @@ def viewImages():
 
 if __name__ == '__main__':
     app.run(debug=True)
-app.secret_key = 'secretkey'
