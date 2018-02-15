dc18bbfeef0ed0f884bb7fceb913941cd5a206d9
Ashutosh Singh <ashu.singh234@gmail.com>
2018-02-15 00:30:40 -0600

    dded import for mysql connector

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from azure.storage.blob import BlockBlobService
 from azure.storage.blob import PublicAccess
 from azure.storage.blob import ContentSettings
+import mysql.connectora
 import os
 
 app = Flask(__name__)
