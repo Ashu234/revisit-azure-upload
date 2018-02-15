5a65610fc8d9ae71f7003508c563deabfa274dbd
Ashutosh Singh <ashu.singh234@gmail.com>
2018-02-15 00:34:15 -0600

    Update main.py
    added session

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from azure.storage.blob import BlockBlobService
 from azure.storage.blob import PublicAccess
 from azure.storage.blob import ContentSettings
@@ -10,8 +10,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+  session['logged_in'] = True
   return render_template('index.html')
 
 
 if __name__ == '__main__':
+    app.secret_key = 'secretkey'
     app.run(debug=True)
+    
