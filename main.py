69f2d0ae7886e72e174e70acadb5897e65ae7572
Ashu234 <ashu.singh234@gmail.com>
2018-02-14 23:26:10 -0600

    added sql and login functionality

--- main.py
+++ main.py
@@ -2,20 +2,74 @@ from flask import Flask, render_template, request, redirect, url_for, session, l
 from azure.storage.blob import BlockBlobService
 from azure.storage.blob import PublicAccess
 from azure.storage.blob import ContentSettings
+import mysql.connector
+from mysql.connector import errorcode
 import os
 
 app = Flask(__name__)
+
+config = {
+  'host':'myserver-mysql-ashu.mysql.database.azure.com',
+  'user':'root123@myserver-mysql-ashu',
+  'password':'Superman123',
+  'database':'mysqlashudb',
+  'ssl_ca':'BaltimoreCyberTrustRoot.crt.pem'
+}
 
 block_blob_service = BlockBlobService(account_name='ashuazurestorage', account_key='HGvsHgPPFOp64gztvR6B9g+RNUUqzwhl+aNid8wpwca1uwejBMEhyVkP3oev1SKEnI5eeq4EIXWfcvzWjxAjuQ==')
 block_blob_service.set_container_acl('ashu-blob-container', public_access=PublicAccess.Container)
 
 @app.route('/')
-def hello_world():
-  return render_template('home.html')
+def index():
+  return redirect(url_for('login'))
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        print(username)
+        session['logged_in'] = True
+        session['username'] = username
+        return redirect(url_for('dashboard'))
+    return render_template('login.html')
+
+@app.route('/dashboard')
+def dashboard():
+  return render_template('dashboard.html')
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
+        username = session['username']
+        try:
+           conn = mysql.connector.connect(**config)
+           print("Connection established")
+        except mysql.connector.Error as err:
+          if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with the user name or password")
+          elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+          else:
+            print(err)
+        else:
+          cursor = conn.cursor()
+          for file in request.files.getlist("file"):
+                title = request.form['title']
+                file_to_upload = file.filename
+                print('----===================-------------------------')
+                print(os.path.dirname(__file__))
+                full_path_to_file = os.path.join(os.path.dirname(__file__), file_to_upload)
+                block_blob_service.create_blob_from_path(
+                'ashu-blob-container',
+                title,
+                full_path_to_file,
+                content_settings=ContentSettings(content_type='image/png')
+                )
+                cursor.execute("INSERT INTO images (username, title) VALUES (%s, %s);", (username, title))
+                print("Inserted",cursor.rowcount,"row(s) of data.")
+          conn.commit()
+          cursor.close()
+          conn.close()
         return render_template('complete.html')
     return render_template('upload.html')
 
@@ -53,4 +107,5 @@ def viewImages():
     return render_template('viewImages.html', blobs=blobs)
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.secret_key = 'secretkey'
+    app.run(debug=True)
