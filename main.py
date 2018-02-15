b5c4bf51e897d761acfe5daf963ff58ed75da046
Ashutosh Singh <ashu.singh234@gmail.com>
2018-02-15 01:19:22 -0600

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def index():
     #cursor.execute("INSERT INTO images (username, title) VALUES (%s, %s);", (username, title))
     result = cursor.execute("SELECT * FROM images;")
     rows = cursor.fetchall()
-    return render_template('viewdata.html')
+    return render_template('viewdata.html', rows=rows)
     cursor.close()
     conn.close()
   return render_template('index.html')
