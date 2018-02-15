dfdd903ab8a82f6669475b831dcf8c0406180909
Ashutosh Singh <ashu.singh234@gmail.com>
2018-02-15 00:13:09 -0600

    Update main.py
    deleted all contenets

--- main.py
+++ main.py
@@ -3,73 +3,15 @@ from azure.storage.blob import BlockBlobService
 from azure.storage.blob import PublicAccess
 from azure.storage.blob import ContentSettings
 import mysql.connector
-from mysql.connector import errorcode
 import os
 
 app = Flask(__name__)
 
-config = {
-  'host':'myserver-mysql-ashu.mysql.database.azure.com',
-  'user':'root123@myserver-mysql-ashu',
-  'password':'Superman123',
-  'database':'mysqlashudb',
-  'ssl_ca':'BaltimoreCyberTrustRoot.crt.pem'
-}
-
-block_blob_service = BlockBlobService(account_name='ashuazurestorage', account_key='HGvsHgPPFOp64gztvR6B9g+RNUUqzwhl+aNid8wpwca1uwejBMEhyVkP3oev1SKEnI5eeq4EIXWfcvzWjxAjuQ==')
-block_blob_service.set_container_acl('ashu-blob-container', public_access=PublicAccess.Container)
 
 @app.route('/')
 def index():
   return render_template('index.html')
 
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        return redirect(url_for('dashboard'))
-    return render_template('login.html')
-
-@app.route('/dashboard')
-def dashboard():
-  return render_template('dashboard.html')
-
-@app.route('/upload', methods=['GET', 'POST'])
-def upload():
-    if request.method == 'POST':
-        return render_template('complete.html')
-    return render_template('upload.html')
-
-@app.route('/rating/<string:title>/', methods=['POST','GET'])
-def rating(title):
-    if request.method == 'POST':
-      rating = request.form['rating']
-      return render_template('complete.html', rating=rating)
-    return render_template('rating.html', title=title)
-
-
-class MyBlob(object):
-    url = ''
-    size = 0
-    title = ''
-    date = ''
-
-    def __init__(self, url, size, title, date):
-        self.url = url
-        self.size = (size/1024)
-        self.title = title
-        self.date = date
-
-
-@app.route('/viewImages', methods=['GET', 'POST'])
-def viewImages():
-    blobs = []
-    generators = block_blob_service.list_blobs('ashu-blob-container')
-    for blob in generators:
-        blob_url = block_blob_service.make_blob_url('ashu-blob-container', blob.name, protocol=None, sas_token=None)
-        myBlob = MyBlob(blob_url,blob.properties.content_length,blob.name,blob.properties.last_modified)
-        blobs.append(myBlob)
-    return render_template('viewImages.html', blobs=blobs)
 
 if __name__ == '__main__':
     app.run(debug=True)
