e288dbb9cfc5bf037710a8bdbce9a75dbf42c3ec
Ashutosh Singh <ashu.singh234@gmail.com>
2018-02-15 00:48:42 -0600

    Update main.py
    adding mysql config

--- main.py
+++ main.py
@@ -3,14 +3,37 @@ from azure.storage.blob import BlockBlobService
 from azure.storage.blob import PublicAccess
 from azure.storage.blob import ContentSettings
 import mysql.connector
+from mysql.connector import errorcode
 import os
 
 app = Flask(__name__)
+
+config = {
+  'host':'myserver-mysql-ashu.mysql.database.azure.com',
+  'user':'root123@myserver-mysql-ashu',
+  'password':'Superman123',
+  'database':'mysqlashudb',
+  'ssl_ca':'BaltimoreCyberTrustRoot.crt.pem'
+}
 
 
 @app.route('/')
 def index():
   session['logged_in'] = True
+  try:
+     conn = mysql.connector.connect(**config)
+     print("Connection established")
+  except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+      print("Something is wrong with the user name or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+      print("Database does not exist")
+    else:
+      print(err)
+  else:
+    cursor = conn.cursor()
+    cursor.close()
+    conn.close()
   return render_template('index.html')
 
 
